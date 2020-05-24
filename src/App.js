cb1515413e1b37c62e63ffe52445dd5e418938a2
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-24 14:09:26 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Currencies from './components/Currencies';
 
 function App() {
   return (
     <div className="App">
       <h1>Currencies</h1>
+
+      <Currencies />
       
     </div>
   );
