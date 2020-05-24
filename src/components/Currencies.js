43c4f1c0fc5060dd605425ae5bae199f7a3fb916
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-24 15:23:59 -0500

    updated api url

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ReactTable from "react-table";
+
 
 
 export default class Currencies extends Component {
@@ -7,49 +7,50 @@ export default class Currencies extends Component {
         super(props);
 
         this.state ={
-            currencies : []
+            isLoading: true,
+            currencies: []
         };
     }
 
-
-    componentDidMount(){
-        fetch('http://localhost:8080/ForexSupplement_api/v1/currencies')
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({
-                    currencies: responseData
-                })
-            })
-            .catch(err => console.error(err))
-
-    }
-
-    render() {
-        const columns = [{
-            Header: 'Currency',
-            accessor: 'currencyName',
-            Cell: this.editable
-        },{
-            Header: 'Code',
-            accessor: 'currencyCode',
-            Cell: this.editable
-
-        }, {
-            Header: 'Amount',
-            accessor: 'currencyAmt'
-        }, {
-            Header: 'Rate',
-            accessor: 'currencyRate'
+        async componentDidMount(){
+            //grab url use for Request Mapping in Eclipse
+            const response = await fetch('http://localhost:8080/ForexSupplement_api/v1/currencies');
+            const body = await response.json();
+            this.setState({
+                currencies:body, 
+                isLoading: false
+            });
         }
-    ,];
-        
+    
+    render() {
+        const {currencies,isLoading} =this.state;
 
+        if(isLoading){
+            return<p>Loading...</p>
+        }
         return (
-            <div>
-               <ReactTable data={this.state.currencies} 
-               columns={columns} filterable={true} />
-                
-            </div>
-        );
+            <div className ="currencyContainer">
+                <h2>Employee List</h2>
+                {currencies.map(currency =>
+                  <div key={currency.id} className="currencyParent">
+                      <table className="currencyTable">
+                        <tr>
+                          <th>Currency Name</th>
+                          <th>Currency Code</th>
+                          <th>Amount</th>
+                          <th>Rate</th>
+                        </tr>
+                        <tr>
+                          <td>{currency.currencyName}</td>
+                          <td>{currency.currencyCode}</td>
+                          <td>{currency.currencyAmt}</td>
+                          <td>{currency.currencyRate}</td>
+                        </tr>
+                      </table>
+                  </div>
+                )}
+
+          </div>
+        )
     }
 }
