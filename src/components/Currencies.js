ff6e5a294a48d1883bde34a58b459213b6652c64
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-24 16:24:39 -0500

    edits to get data to render

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ReactTable from "react-table";
+
 
 
 
@@ -22,35 +24,29 @@ export default class Currencies extends Component {
             });
         }
     
-    render() {
-        const {currencies,isLoading} =this.state;
-
-        if(isLoading){
-            return<p>Loading...</p>
-        }
-        return (
-            <div className ="currencyContainer">
-                <h2>Employee List</h2>
-                {currencies.map(currency =>
-                  <div key={currency.id} className="currencyParent">
-                      <table className="currencyTable">
-                        <tr>
-                          <th>Currency Name</th>
-                          <th>Currency Code</th>
-                          <th>Amount</th>
-                          <th>Rate</th>
-                        </tr>
-                        <tr>
-                          <td>{currency.currencyName}</td>
-                          <td>{currency.currencyCode}</td>
-                          <td>{currency.currencyAmt}</td>
-                          <td>{currency.currencyRate}</td>
-                        </tr>
-                      </table>
-                  </div>
-                )}
-
-          </div>
-        )
+        render() {
+    
+            const columns = [{
+                Header: 'Currency',
+                accessor: 'currencyName',
+                Cell: this.editable
+            }, {
+                Header: 'Code',
+                accessor: 'currencyCode',
+                Cell: this.editable
+            }, {
+                Header: 'Amount',
+                accessor: 'currencyAmount'
+            }, {
+                Header: 'Rate',
+                accessor: 'currencyRate'
+            }
+                ,];
+        
+            return (
+                <div>
+                    <ReactTable data={this.state.currencies} columns={columns} filterable={true}/>
+                </div>
+            );
+        }  
     }
-}
