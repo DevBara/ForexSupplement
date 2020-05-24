3a584f8a2c6c327344b7fca37571a018a5b78b88
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-24 15:24:39 -0500

    added links,routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Currencies from './components/Currencies';
+import WorldNews from './components/WorldNews'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Currencies</h1>
-
-      <Currencies />
-      
-    </div>
-  );
+    <div>
+      <Router>
+        <div className="App">
+          <nav className="navBar">
+            <ul>
+              <li>
+                <Link className="link" to ="/home"> Home </Link>
+              </li>
+              <li>
+                <Link className="link" to ="/currencies">Tracked Currencies</Link>
+              </li>
+              <li>
+                <Link className="link" to ="/news">World News</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/currencies" component={Currencies} />
+            <Route path="/news" component={WorldNews} />
+          </Switch>
+        </div>
+    </Router>
+   </div>
+  )
+  
 }
+
 
 export default App;
